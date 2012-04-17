2c39f04e827938aea6266fe9b0853e40bbc94744
jriby <julien.riby@etu.enseeiht.fr>
2012-04-17 11:44:53 +0200

    Scenario 1 : liste des posts - OK

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ProjetBlog::Application.routes.draw do
+
+match '/posts' => 'posts#index', :as => :posts, :via => :get
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,6 +53,8 @@ ProjetBlog::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  
+  #root :to => 'posts#index'
 
   # See how all your routes lay out with "rake routes"
 
