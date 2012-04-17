77c1f1237d7a35e2f8ad723bfa4c7953e344c077
jriby <julien.riby@etu.enseeiht.fr>
2012-04-17 09:48:15 +0200

    projet blog créé

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjetBlog::Application.config.secret_token = '43a339c47c57bb69d40fe2459e1eaa8bd6398031bd2af531bc0123716198ea263bd982d3fc115e5561578bb9e0ea874ac1bd9ba8b692852da363ec5460b2e8f3'
